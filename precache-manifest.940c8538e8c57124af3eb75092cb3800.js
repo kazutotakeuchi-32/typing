78b843802b1b9b9676eb32b7f1aa36b2a8289621
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-08-14 16:27:18 +0900

    Updates

---
+++ precache-manifest.940c8538e8c57124af3eb75092cb3800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4bdc65cb12f1bd15e34a859f512729",
+    "url": "/typing/index.html"
+  },
+  {
+    "revision": "e05e98a9227083d5c07a",
+    "url": "/typing/static/css/main.7ddb044f.chunk.css"
+  },
+  {
+    "revision": "d76f5bb09ba5be025503",
+    "url": "/typing/static/js/2.98f1b96f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing/static/js/2.98f1b96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05e98a9227083d5c07a",
+    "url": "/typing/static/js/main.12d58db7.chunk.js"
+  },
+  {
+    "revision": "c5f64a8d2403e5f89708",
+    "url": "/typing/static/js/runtime-main.175ab308.js"
+  },
+  {
+    "revision": "2e76521c5e8a26369b3e2996ba192a24",
+    "url": "/typing/static/media/DSEG7Classic-Regular.2e76521c.ttf"
+  }
+]);
